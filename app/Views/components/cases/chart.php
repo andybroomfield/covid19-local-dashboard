53d7aa740c81593bb1c5c991f9a7d32f69c5ab7c
Andy Broomfield <andybroomfield@gmail.com>
2021-06-20 21:16:27 +0100

    Add basic styles for the area case history page
    - Add flex styles for summary
    - Add padding and margin for chart
    - Add stripped table for area case history

--- app/Views/components/cases/chart.php
+++ app/Views/components/cases/chart.php
@@ -10,9 +10,9 @@
   $date_str = rtrim($date_str, ', ');
 ?>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@3.3.2/dist/chart.min.js" integrity="sha256-qoN08nWXsFH+S9CtIq99e5yzYHioRHtNB9t2qy1MSmc=" crossorigin="anonymous"></script>
-<canvas id="myChart" width="400" height="400"></canvas>
+<canvas id="cases-chart" class="cases-chart" width="400" height="400"></canvas>
 <script>
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('cases-chart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
--- app/Views/components/cases/table.php
+++ app/Views/components/cases/table.php
@@ -1,15 +1,17 @@
-<table>
-  <thead>
-    <tr>
-      <th>Date</th><th>Cases</th><th>Cumlitive</th>
+<table class="cases-table">
+  <thead class="cases-table__header">
+    <tr class="cases-table__row cases-table__row--header">
+      <th class="cases-table__date cases-table__date--header">Date</th>
+      <th class="cases-table__cases cases-table__cases--header">Cases</th>
+      <th class="cases-table__cumlitive cases-table__cumlitive--header">Cumlitive</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody class="cases-table__body">
     <?php foreach ($cases as $case): ?>
-      <tr>
-        <td><?php echo esc($case['date']); ?></td>
-        <td><?php echo esc($case['daily']); ?></td>
-        <td><?php echo esc($case['cumlitive']); ?></td>
+      <tr class="cases-table__row">
+        <td class="cases-table__date"><?php echo esc($case['date']); ?></td>
+        <td class="cases-table__cases"><?php echo esc($case['daily']); ?></td>
+        <td class="cases-table__cumlitive"><?php echo esc($case['cumlitive']); ?></td>
       </tr>
     <?php endforeach; ?>
   </tbody>
